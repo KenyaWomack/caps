1e6318be4f4461e1270e019f91c9eea3500e4f5f
Kenya Womack <ladykamillion27@gmail.com>
2023-05-31 01:17:25 -0500

    update

--- vendor/index.js
+++ vendor/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const { orderHandler, deliveredMessage }= require('./handler');
-const eventEmitter = require('../eventPool');
+// const { orderHandler, deliveredMessage }= require('./handler');
+// const eventEmitter = require('../eventPool');
 
 // starts the event cycle, note that the pickup emit is inside the orderHandler
-setInterval(() => {
-  orderHandler();
-}, 5000);
+// setInterval(() => {
+//   orderHandler();
+// }, 5000);
 
-eventEmitter.on('delivered', deliveredMessage);
+// eventEmitter.on('delivered', deliveredMessage);
 
-// const { io } = require('socket.io-client');
-// const socket = io('http://localhost:3001/caps');
+const { io } = require('socket.io-client');
+const socket = io('http://localhost:3001/caps');
 
 // socket.on and handlers, and emits go here
 
